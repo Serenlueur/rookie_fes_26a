c70dad751bb3bcbab516a2338c9f2344872a58dd
Serenlueur <326974250+Serenlueur@users.noreply.github.com>
2026-09-12 15:38:17 +0900

    deploy setting change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // GitHub Pages: https://<ユーザー名>.github.io/<リポジトリ名>/ で公開される想定
+  site: 'https://serenlueur.github.io',
+  base: '/rookie_fes_26a/',
+});
